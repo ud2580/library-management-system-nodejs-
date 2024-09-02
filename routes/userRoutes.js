83c947f5a630429ccaf4fc87069fdead6805086f
ud2580 <129142102+ud2580@users.noreply.github.com>
2024-09-02 18:33:30 +0530

    delete not useble code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -35,7 +35,7 @@ router.post('/login', async (req, res) => {
   if (!user || !await bcrypt.compare(password, user.password)) {
     return res.status(401).send('Invalid credentials');
   }
-  const token = jwt.sign({ userId: user._id, role: user.role }, 'secret-key');
+  const token = jwt.sign({ userId: user._id}, '12345');
   res.send({ token });
   }catch (err) {
     console.log(err);
@@ -43,26 +43,6 @@ router.post('/login', async (req, res) => {
 }
 });
 
-router.put('/profile/password',jwtAuthMiddleware, async (req, res)=>{
-  try{
-      const userId = req.user; // Extract the id from the URL parameter
-      const  {currentpassword,newpassword}=reb.body;
 
-      const user = await user.findOne(userId);
-      if( !(await user.comparePassword(password))){
-          return res.status(401).json({error: 'Invalid username or password'});
-      }
-
-      user.password=newpassword;
-      await user.save();
-
-
-      console.log('password updated');
-      res.status(200).json({message: "password updated"});
-  }catch(err){
-      console.log(err);
-      res.status(500).json({error: 'Internal Server Error'});
-  }
-})
 
 module.exports = router;
